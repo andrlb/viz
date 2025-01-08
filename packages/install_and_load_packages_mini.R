2772618fbe3b5b69acba8e6826268662da54f0b5
André Luiz <andreluizmtg@gmail.com>
2025-01-08 20:12:01 -0300

    8/1

---
+++ packages/install_and_load_packages_mini.R
@@ -0,0 +1,8 @@
+packages <- c('janitor', 'ggplot2', 'readxl', 'writexl')
+
+for (pkg in packages) {
+  if (!require(pkg, character.only = TRUE)) {
+    install.packages(pkg, method = "wininet")
+    library(pkg, character.only = TRUE)
+  } 
+}
